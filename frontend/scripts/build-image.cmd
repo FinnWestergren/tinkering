c345a6d73305be845ce10dec3472b1acdd0164b8
Finn Westergren <fwesterg5@gmail.com>
2021-08-24 17:24:01 -0400

    dockerized

---
+++ frontend/scripts/build-image.cmd
@@ -0,0 +1,3 @@
+pushd C:\Users\fwest\Documents\GitHub\tinkering\frontend 
+docker build -t tinker-town . 
+popd
---
+++ frontend/scripts/run-image.cmd
@@ -0,0 +1,3 @@
+pushd C:\Users\fwest\Documents\GitHub\tinkering\frontend 
+docker run -dp 8080:8080 tinker-town
+popd
